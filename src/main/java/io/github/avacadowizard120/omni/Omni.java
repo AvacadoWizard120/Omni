42f844bf527d9508c87b08513d06b3388d46be3f
AvacadoWizard120 <edenjudeurias@gmail.com>
2024-08-06 10:02:52 -0500

    Support 1.20.6

--- src/main/java/io/github/avacadowizard120/omni/Omni.java
+++ src/main/java/io/github/avacadowizard120/omni/Omni.java
@@ -8,25 +8,28 @@ import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Omni.MODID)
-public class Omni {
+public class Omni
+{
     private static final double SPRINT_SPEED = 0.28;
 
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "omni";
 
-    public Omni() {
+    public Omni()
+    {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     @SubscribeEvent
-    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        if (event.phase == TickEvent.Phase.END && event.player.level().isClientSide) {
+    public void onPlayerTick(TickEvent.PlayerTickEvent event)
+    {
+        if (event.phase == TickEvent.Phase.END && event.player.level().isClientSide)
+        {
             Player player = event.player;
             Minecraft mc = Minecraft.getInstance();
 
-            if (mc.options.keySprint.isDown()) {
+            if (mc.options.keySprint.isDown())
+            {
                 double forward = 0;
                 double strafe = 0;
 
@@ -35,7 +38,8 @@ public class Omni {
                 if (mc.options.keyLeft.isDown()) strafe++;
                 if (mc.options.keyRight.isDown()) strafe--;
 
-                if (forward != 0 || strafe != 0) {
+                if (forward != 0 || strafe != 0)
+                {
                     // Normalize diagonal movement
                     float inputMagnitude = (float) Math.sqrt(forward * forward + strafe * strafe);
                     forward /= inputMagnitude;
