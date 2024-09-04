7d29dd92a2e1327e11515e2351323851d19f7dd2
AvacadoWizard120 <edenjudeurias@gmail.com>
2024-09-04 15:47:52 -0500

    init 1.20 Fabric

---
+++ src/main/java/io/github/AvacadoWizard120/omni/Omni.java
@@ -0,0 +1,10 @@
+package io.github.AvacadoWizard120.omni;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Omni implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
